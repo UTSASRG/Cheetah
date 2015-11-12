3b94794217e5c7c88a1e1d4bc7705a2f2feac767
tongping <tongping.liu@utsa.edu>
2015-11-11 19:58:33 -0600

    Update the libnewptheread

--- source/newpthreads/libnewpthreads.cpp
+++ source/newpthreads/libnewpthreads.cpp
@@ -8,6 +8,18 @@ extern "C" {
 	#define HARDWARE_CORES_NUM 16
 	unsigned long threadIndex = 0;
 
+  typedef void * threadFunction (void *);
+
+	typedef struct {
+		threadFunction * startRoutine;
+  	void * startArg;
+		int    threadIndex;
+	} startStruct; 
+		
+  startStruct mythread;
+
+
+
 	void initializer (void) __attribute__((constructor));
 
 	// Setting up the first thread.
@@ -19,6 +31,20 @@ extern "C" {
 
       pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 	}
+
+	void * startThread(void * arg) {
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+
+    CPU_SET((mythread.threadIndex%HARDWARE_CORES_NUM)*2, &cpuset);
+
+    // Seting up the affinity
+    pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+
+		mythread.startRoutine(mythread.startArg);
+	
+		return NULL;	
+	}
 	  
 	// Intercept the pthread_create function.
   int pthread_create (pthread_t * tid, const pthread_attr_t * attr, void *(*start_routine) (void *), void * arg)
@@ -26,19 +52,12 @@ extern "C" {
   	typedef int (*real_pthread_create) (pthread_t * tid, const pthread_attr_t * attr, void *(*start_routine) (void *), void * arg);
 
     static real_pthread_create thread_create  = (real_pthread_create) dlsym(RTLD_NEXT, "pthread_create");
-		
-		int result = thread_create(tid, attr, start_routine, arg);
-
-		if(result == 0) {
-      cpu_set_t cpuset;
-      CPU_ZERO(&cpuset);
-
-      CPU_SET(threadIndex++%HARDWARE_CORES_NUM, &cpuset);
-
-      // Seting up the affinity
-      pthread_setaffinity_np(*tid, sizeof(cpu_set_t), &cpuset);
-    }
-		return result;
+	
+		mythread.startRoutine = start_routine;
+		mythread.startArg = arg;
+		mythread.threadIndex = __atomic_fetch_add(&threadIndex, 1, __ATOMIC_RELAXED);
+ 
+		return thread_create(tid, attr, startThread, NULL);
   }
 };
 
